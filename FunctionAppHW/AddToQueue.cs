73a9ac21bdcb8c4258a98efa1dec5af68640d3aa
Sevgi <sevgi.elesgerova@gmail.com>
2024-12-07 00:41:56 +0400

    Add project files.

---
+++ FunctionAppHW/AddToQueue.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Storage.Queues;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionAppHW
+{
+    public static class AddToQueue
+    {
+        [Function("AddToQueue")]
+        public static async Task Run(
+            [TimerTrigger("*/5 * * * * *")] TimerInfo myTimer,
+            ILogger log)
+        {
+            string connectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            string queueName = "productqueue";
+            string message = $"Product-{Guid.NewGuid()}";
+
+            QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+            await queueClient.CreateIfNotExistsAsync();
+
+            await queueClient.SendMessageAsync(message);
+
+            log.LogInformation($"Added message to queue: {message} at {DateTime.Now}");
+        }
+    }
+}
---
+++ FunctionAppHW/ReadAndDeleteFromQueue.cs
@@ -0,0 +1,42 @@
+﻿using Azure.Storage.Queues.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionAppHW
+{
+    public static class ReadAndDeleteFromQueue
+    {
+        [Function("ReadAndDeleteFromQueue")]
+        public static async Task Run(
+            [TimerTrigger("*/7 * * * * *")] TimerInfo myTimer,
+            ILogger log)
+        {
+            string connectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            string queueName = "productqueue";
+
+            var queueClient = new Azure.Storage.Queues.QueueClient(connectionString, queueName);
+
+            if (await queueClient.ExistsAsync())
+            {
+                QueueMessage[] messages = await queueClient.ReceiveMessagesAsync(maxMessages: 1);
+
+                foreach (var message in messages)
+                {
+                    log.LogInformation($"Read message: {message.MessageText}");
+
+                    await queueClient.DeleteMessageAsync(message.MessageId, message.PopReceipt);
+                    log.LogInformation($"Deleted message with ID: {message.MessageId}");
+                }
+            }
+            else
+            {
+                log.LogWarning($"Queue '{queueName}' does not exist.");
+            }
+        }
+    }
+}
